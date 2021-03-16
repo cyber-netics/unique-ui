b092506789bc326b4a23aa416547480477fb82e9
Eduard  Jacobs <eduardjacobs@Eduards-MacBook-Pro.local>
2021-03-16 12:07:21 -0700

    test

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { darkTheme } from "../../theme";
+import Navigation from "../Nav";
 
 const Layout = ({ children }) => {
   let theme = darkTheme;
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Navigation />
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,41 @@
 import React from "react";
-import Navigation from "../components/Nav";
+import styled from "styled-components";
+
 import Particles from "../components/Particle";
+import bgimage from "../img/bg-primary.png";
+import Bgsecimg from "../assets/svg/bg.svg";
+
+const Page = styled.div`
+  border-color: transparent !important;
+  background: #230080;
+  background: linear-gradient(135deg, #230080, #a614a3 70%, #e31eb4);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#230080",endColorstr="#e31eb4",GradientType=1);
+  z-index: 0;
+`;
+
+const PageBackground = styled.div`
+  background-image: url(${bgimage});
+  background-attachment: scroll;
+  background-clip: border-box;
+  background-origin: padding-box;
+  background-position: 0 0;
+  background-repeat: no-repeat;
+  background-size: 100% auto;
+  bottom: 0;
+  content: "";
+  opacity: 0.5;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+`;
 
 const IndexPage = () => {
   return (
-    <>
-      <Navigation />
+    <Page>
+      <PageBackground />
       <Particles />
-      <h1>hello</h1>
-    </>
+      <Bgsecimg />
+    </Page>
   );
 };
 
